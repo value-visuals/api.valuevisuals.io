545d2e77e8cb8daac5b8fd400151dcf2d62dc463
B3PO <me@b3po.io>
2026-08-14 16:48:40 -0700

    Update firebase.js
    Testing changes to deploy to digitalocean App Platform

--- config/firebase.js
+++ config/firebase.js
@@ -1,15 +1,18 @@
 // config/firebase.js
 import dotenv from "dotenv";
-dotenv.config(); // ✅ ensures .env is loaded, no matter import order
+dotenv.config();
 
 import admin from "firebase-admin";
 
-// pull from env
 const bucketName = process.env.STORAGE_BUCKET;
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
     ...(bucketName ? { storageBucket: bucketName } : {}),
   });
 }
